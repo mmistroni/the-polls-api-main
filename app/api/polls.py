d076adc28e3456c250bfcb7553dcd6670addaf55
mmistroni <mmistroni@gmail.com>
2025-12-08 06:52:09 +0000

    firs tpart of capstone

---
+++ app/api/polls.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel,Field
+from datetime import datetime
+import uuid
+from typing import List
+
+class Poll:
+    id:uuid.UUID = Field(description="Unique id", default_factory=lambda : uuid.UUID())
+    options:List[str]
+    created:datetime = Field(description='Created Time', default=datetime.now())
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
-from app.api import polls, votes, danger, exceptions
+from app.api import polls
+import uvicorn
+#from app.api import votes, danger, exceptions
 
 
 app = FastAPI(
@@ -24,14 +26,22 @@ app = FastAPI(
 )
 
 
+'''
 app.add_exception_handler(
     RequestValidationError, exceptions.custom_validation_exception_handler
 )
 app.include_router(polls.router, prefix="/polls", tags=["polls"])
 app.include_router(danger.router, prefix="/polls", tags=["danger"])
 app.include_router(votes.router, prefix="/vote", tags=["votes"])
-
+'''
 
 # Objective:
 # - reshape the pydantic error messages into custom messages that only
 # include the "msg" property of the error
+
+@app.get('/test')
+def test():
+    return {'message' : 'Hello there'}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
