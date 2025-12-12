c81d1c849b650b3c6fab5fea407652b2ad078628
mmistroni <mmistroni@gmail.com>
2025-12-12 06:11:19 +0000

    c

--- app/api/polls.py
+++ app/api/polls.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import uuid
 from typing import List
 
-class Poll:
+class Poll(BaseModel):
     id:uuid.UUID = Field(description="Unique id", default_factory=lambda : uuid.UUID())
     options:List[str]
     created:datetime = Field(description='Created Time', default=datetime.now())
