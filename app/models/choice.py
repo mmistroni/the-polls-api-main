06a6887dae936a2b7db846e285f4f1c2061e0025
mmistroni <mmistroni@gmail.com>
2025-12-24 07:07:55 +0000

    updating api

---
+++ app/models/choice.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel,Field
+from datetime import datetime, timezone
+from uuid import UUID, uuid4
+from typing import List, Optional
+
+class ChoiceCreate(BaseModel):
+    """ Choice write data model representing a single choice in a poll"""
+    description:str = Field
+
+class ChoiceCreate(BaseModel):
+    """ Choice read data model wih a label and an auto gen uuid"""
+    id:UUID = Field(description="Unique id", default_factory=uuid4)
+    label:int = Field(description="Label", gt=0, le=5)
