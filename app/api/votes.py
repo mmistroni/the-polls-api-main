eb1a4200105e0e580c920f8624c5498478500f18
mmistroni <mmistroni@gmail.com>
2026-03-23 06:07:01 +0000

    updated model

--- app/api/votes.py
+++ app/api/votes.py
@@ -10,16 +10,28 @@ def vote_by_id(poll_id:UUID, vote_by_id:VoteByID):
     logging.info('=== Voting By Id====')
     
     logging.info('Saving into redis...')
+    vote = Vote(poll_id=poll_id, 
+                choice_id=vote_by_id.choice_id,
+                voter = VoterCreate())
+
+    return vote.model_dump_json()
+
+
     #utils.save_poll(new_poll)
     
-    return {"message": "Vote recorded"}
+    return vote_by_id.model_dump_json()
     
 @router.post('/vote/{poll_id}/label')
 def vote_by_label(poll_id:UUID, vote_by_label:VoteByLabel):
     logging.info('=== Voting By Label====')
-    
     logging.info('Saving into redis...')
-    
-    return {"message": "Vote recorded"}
+    choice_id = utils.get_choice_id_by_label(poll_id, 
+                                             vote_by_label.choice_label
+                                             )
+    vote = Vote(poll_id=poll_id, 
+                choice_id=choice_id,
+                voter = VoterCreate())
+
+    return vote.model_dump_json()
 
 
